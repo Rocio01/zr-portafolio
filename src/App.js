20a87ac6f1c3acd98399783d78c3868a74a1c5ab
Zulma Rocio <zrmartinezg@gmail.com>
2022-02-14 18:14:38 -0500

    Add introduction section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import Introduction from './components/Introduction';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Introduction />
     </div>
   );
 }
---
+++ src/components/Introduction.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Introduction = () => {
+  return (
+    <section className='intro'>
+      <h1 className='section-title section-title-intro'>
+       Hello there, I'm  <strong> Zulma Rocio </strong>      
+      </h1>
+      <p className='section-subtitle section-subtitle-intro'>
+        I'm Software developer <br />       
+        I would love to help you build your product, <br />    
+        feature, website, feel free to contact me.        
+      </p>
+    </section>
+  )
+}
+
+export default Introduction
