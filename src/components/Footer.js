4429f66268e4bdc736ce93eeecb7cb0eb4c80f99
Zulma Rocio <zrmartinezg@gmail.com>
2022-02-20 20:27:04 -0500

    Add style to footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,15 @@
 import React from 'react'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faClock, // the clock icon
-  faUserCircle, // the user circle icon
-  faCoffee, // a cup of coffee
-} from "@fortawesome/free-solid-svg-icons";
-
+ import { FaGithub, FaTwitter, FaLinkedin} from "react-icons/fa";
 const Footer = () => {
   return (
     <footer>
     <a href="mailto:zrmartinezg@gmail.com" className="footer-link">zrmartinezg@gmail.com</a>
     <ul className="social-list">
-        <li className="social-list-item"><a className="social-list-link" href="https://codepen.io">a</a></li>
-        <li className="social-list-item"><a className="social-list-link" href="http://dribbble.com">b</a></li>
-        <li className="social-list-item"><a className="social-list-link" href="https://twitter.com">c</a></li>
-        <li className="social-list-item"><a className="social-list-link" href="https://github.com">d</a></li>
-    </ul>
-    <FontAwesomeIcon
-          icon={faClock}
-          style={{ fontSize: 100, color: "blue", marginRight: 30 }}
-        />
+        <li className="social-list-item"><a className="social-list-link" href="https://github.com/Rocio01"><FaGithub /* style={{ fontSize: 100, color: "blue", marginRight: 30 }}*/ /> </a></li> 
+        <li className="social-list-item"><a className="social-list-link" href="https://twitter.com/rugiada8801"> <FaTwitter/></a></li>
+        <li className="social-list-item"><a className="social-list-link" href="https://www.linkedin.com/in/zulma-rocio-martinez"><FaLinkedin /></a></li>
+        
+    </ul>    
 </footer>
   )
 }
