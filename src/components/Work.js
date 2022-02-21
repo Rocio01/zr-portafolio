abd03e200ff988f07293ddf2a4f1661a963d5074
Zulma Rocio <zrmartinezg@gmail.com>
2022-02-21 17:18:38 -0500

    Update components with redux

--- src/components/Work.js
+++ src/components/Work.js
@@ -3,33 +3,29 @@ import ReactCapstone from '../images/react-capstone.PNG'
 import RailsCapstone from '../images/rails-time-trac.PNG'
 import JsProject from '../images/js-project.PNG'
 import Copito from '../images/copito-bot.PNG'
+
 import { useSelector } from 'react-redux';
 const Work = () => {
 
     const worksState = useSelector((state) => state.worksReducer.works)
-    console.log(worksState)
+    const pokemon = ReactCapstone;
+    const rails = RailsCapstone;
+    const restaurant = JsProject;
+    const copito = Copito;
 
-    
   return (
     <section className="my-work" id="work">
     <h2 className="section-title section-title-work">My work</h2>
     <p className="section-subtitle section-subtitle-work">Some examples of my work</p>
     
     <div className="portfolio">
-        
-        <a href="#" className="portfolio-item">
-            <img src={ReactCapstone} alt="" className="portfolio-img" />
+        {worksState.map((work) => (
+           
+        <a href="#work" className="portfolio-item" key={work.id}>
+            <img src={eval(work.imageName)} alt="" className="portfolio-img" key={work.id}/>
         </a>
-        <a href="#" className="portfolio-item">
-            <img src={RailsCapstone} alt="" className="portfolio-img" />
-        </a>
-        <a href="#" className="portfolio-item">
-            <img src={JsProject} alt="" className="portfolio-img" />
-        </a>
-        <a href="#" className="portfolio-item">
-            <img src={Copito} alt="" className="portfolio-img" />
-        </a>
-        
+            
+        ))}        
         
     </div>
 </section>
--- src/redux/my-work/works.js
+++ src/redux/my-work/works.js
@@ -1,34 +1,39 @@
+
 const initialState = {
   works: [{
+            id: 1,
             name: "Pokemon App",
             description: 'Pokemon is a SPA to show pokemons and retrieve information about each one of them. the app displays a list of 30 Pokemons.',
             technologies: 'React, Redux, Bootstrap, CSS3, Heroku, pokeapi',
             demo: 'https://zr-pokemons.herokuapp.com/',
-            image: 'ReactCapstone',
-            github: "https://github.com/Rocio01/pokemon-capstone"
+            github: "https://github.com/Rocio01/pokemon-capstone",
+            imageName: "pokemon"
           },
           {
+            id: 2,
             name: "Time Track App",
             description: 'This is an application where you can group your activities and track how many hours you spend on them, you can also create activities without grouping them.',
             technologies: 'Ruby, Ruby on Rails, Bootstrap, CSS3',
             demo: 'https://evening-castle-40558.herokuapp.com/',
-            image: 'RailsCapstone',
+            imageName: "rails",
             github: "https://github.com/Rocio01/capstone-rails"
           },
           {
+            id: 3,
             name: "Restaurant page",
             description: 'A simple restaurant homepage rendered dynamically by DOM manipulation, and the use of modularized code using webpack.',
             technologies: 'HTML5, CSS3, JavaScript, Webpack',
             demo: 'https://rocio01.github.io/restaurant-page/',
-            image: 'JsProject',
+            imageName: "restaurant",
             github: "https://github.com/Rocio01/restaurant-page"
           },
           {
+            id: 4,
             name: "Telegram Bot",
             description: 'In this project, I built a bot using the platform Telegram. This is a bot that will fill your curiosity about our furry friends (dogs and cats), it posts random curiosities about dogs and cats and also images.',
             technologies: 'Ruby, Colorize gem, telegram-bot-ruby gem, rspec gem, Rubocop, telegram',
             demo: '',
-            image: 'Copito',
+            imageName: "copito",
             github: "https://github.com/Rocio01/My-telegram-bot"
           }
           ]
