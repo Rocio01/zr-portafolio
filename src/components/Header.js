ede877a831b025be6b430af67ff1a7f259a05194
Zulma Rocio <zrmartinezg@gmail.com>
2022-02-21 10:39:44 -0500

    Refactor nav functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,6 @@ import '../header.css'
 
 
 const Header = () => {
-  const navLinks = document.querySelectorAll('.nav-link')
-  navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
-})
   return (
     <header >
       <div className="logo">
@@ -20,10 +14,10 @@ const Header = () => {
       </button>
     <nav className='nav'>
       <ul className='nav-list'>
-      <li className="nav-item"><a href="#home" className="nav-link">Home</a></li>
-      <li className="nav-item"><a href="#services" className="nav-link">My Services</a></li>
-      <li className="nav-item"><a href="#about" className="nav-link">About me</a></li>
-      <li className="nav-item"><a href="#work" className="nav-link">My Work</a></li>
+      <li className="nav-item"><a href="#home" className="nav-link" onClick={()=> { document.body.classList.remove('nav-open')}}>Home</a></li>
+      <li className="nav-item"><a href="#services" className="nav-link" onClick={()=> { document.body.classList.remove('nav-open')}}>My Services</a></li>
+      <li className="nav-item"><a href="#about" className="nav-link"onClick={()=> { document.body.classList.remove('nav-open')}}>About me</a></li>
+      <li className="nav-item"><a href="#work" className="nav-link"onClick={()=> { document.body.classList.remove('nav-open')}}>My Work</a></li>
       </ul>
     </nav>
     </header>
